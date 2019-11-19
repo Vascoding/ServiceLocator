9bb4156a6e31558a4cea43a62853b576a3124edf
vascoding <vascodeid@gmail.com>
2019-11-20 01:48:37 +0200

    fix namespaces

--- ServiceLocator/Implementations/Container.cs
+++ ServiceLocator/Implementations/Container.cs
@@ -3,7 +3,7 @@ using System.Linq;
 using System.Reflection;
 using ServiceLocator.Contracts;
 
-namespace ServiceLocator.Implementation
+namespace ServiceLocator.Implementations
 {
     public static class Container
     {
